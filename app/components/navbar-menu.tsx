eef1e00d39da9d26f0dff518323153d8ae238c44
Eko Sutriono <ekosutriono321@gmail.com>
2024-09-27 13:30:18 +0700

    impv; hero

---
+++ app/components/navbar-menu.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useState } from "react";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/app/components/ui/navbar";
+import { cn } from "@/libs/utils";
+
+export function NavbarHome() {
+  return (
+    <div className="relative w-full flex items-center justify-center">
+      <Navbar className="bottom-10" />
+    </div>
+  );
+}
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn(
+        "fixed bottom-10 inset-x-0 max-w-2xl mx-auto z-50",
+        className
+      )}
+    >
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="Services">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/web-dev">Web Development</HoveredLink>
+            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+            <HoveredLink href="/branding">Branding</HoveredLink>
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Products">
+          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+            <HoveredLink href="/web-dev">Web Development</HoveredLink>
+            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+            <HoveredLink href="/branding">Branding</HoveredLink>
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Pricing">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/hobby">Hobby</HoveredLink>
+            <HoveredLink href="/individual">Individual</HoveredLink>
+            <HoveredLink href="/team">Team</HoveredLink>
+            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+          </div>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,11 @@ import Image from "next/image";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { TextPlugin, ScrollTrigger } from "gsap/all";
-import Grow from './components/grow'
-import Members from './components/members'
+import Grow from "./components/grow";
+import Members from "./components/members";
+import { HeroHighlight } from "./components/head";
+import { SparklesCore } from "./components/ui/sparkles";
+import { NavbarHome } from "./components/navbar-menu";
 
 export default function Home() {
   const textRef = useRef(null);
@@ -15,7 +18,7 @@ export default function Home() {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    gsap.registerPlugin(TextPlugin, ScrollTrigger)
+    gsap.registerPlugin(TextPlugin, ScrollTrigger);
 
     gsap.to(textRef.current, {
       duration: 2,
@@ -26,71 +29,90 @@ export default function Home() {
       imgRef.current,
       { x: 0 },
       {
-        x: 200,
+        x: 0,
         scrollTrigger: {
           trigger: containerRef.current,
-          start: 'top 10%',
-          end: 'bottom 90%',
+          start: "top 10%",
+          end: "bottom 90%",
           scrub: true,
         },
       }
-    )
+    );
 
     gsap.fromTo(
       imgRef.current,
-      { scale: 1 },
+      { scale: 1, x: 0 },
       {
         scale: 10,
         scrollTrigger: {
           trigger: containerRef.current,
-          start: 'bottom 70%',
-          end: 'bottom 20%',
+          start: "bottom 70%",
+          end: "bottom 40%",
           scrub: true,
         },
       }
-    )
+    );
 
     gsap.fromTo(
       textRef.current,
-      { x: 0, opacity: 1 },
+      { x: 0, opacity: 1, scale: 0 },
       {
-        x: 700,
-        opacity: 0,
+        x: 0,
+        opacity: 1,
+        scale: 5,
         duration: 2,
         scrollTrigger: {
           trigger: containerRef.current,
-          start: 'top 10%',
-          end: 'bottom center',
+          start: "top 10%",
+          end: "bottom center",
           scrub: true,
         },
       }
-    )
-    
-    gsap.to(
-      mainRef.current,
-      {
-        background: 'blue',
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: 'bottom 50%',
-          end: 'bottom 30%',
-          scrub: true
-        },
-      }
-    )
+    );
+
+    gsap.to(mainRef.current, {
+      background: "black",
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "bottom 50%",
+        end: "bottom 30%",
+        scrub: true,
+      },
+    });
   }, []);
 
   return (
-    <main className="flex min-h-[1200px] flex-col items-center justify-center px-24 pt-12  overflow-hidden" ref={mainRef}>
-      <div className="relative z-[-1] w-screen min-h-screen flex place-items-center" ref={containerRef}>
-        <div className="items-center top-[150px] fixed w-screen pl-[0] pr-[200px]">
-          <div className="flex justify-center items-center overflow-hidden">
-            <Image ref={imgRef} alt="Code" width={300} height={300} src='https://storage.googleapis.com/komerce/assets/icons/icon-dev-white.svg'></Image>
+    <main
+      className="flex min-h-[1200px] flex-col items-center justify-center overflow-hidden"
+      ref={mainRef}
+    >
+      <NavbarHome />
+      <div
+        className="w-full p-0 flex flex-col items-center justify-center"
+        ref={containerRef}
+      >
+        <HeroHighlight>
+          <div className="flex align-middle items-center justify-center">
             <span className="title-page" ref={textRef}>
-              KBVGFRuJ SnAR
+              KBVGFRuJSnAR
             </span>
           </div>
-        </div>
+          <div className="w-[100rem] h-500 relative">
+            <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+            <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+            <SparklesCore
+              background="transparent"
+              minSize={0.4}
+              maxSize={1}
+              particleDensity={1200}
+              className="w-full h-full mt-5"
+              particleColor="#FFFFFF"
+            />
+            <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(800px_200px_at_top,transparent_0%,white)]"></div>
+          </div>
+        </HeroHighlight>
       </div>
       <div className="space h-[200px]"></div>
       <Grow />
